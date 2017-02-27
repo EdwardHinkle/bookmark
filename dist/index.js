8fcc09741ae351bca4bea1bbd97414519035ad75
Edward Hinkle <eddie@meetmaestro.com>
2017-02-26 23:38:54 -0500

    Properly configure package.json so that Goodreads
    wrapper properly exports just by importing the bookmark folder in TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-//export * from './lib/goodreads';
-var goodreads_1 = require("./lib/goodreads");
-var test = new goodreads_1.Goodreads({ key: 'KSXzo3GFar32jnjtiVlQjw', secret: 'hP7UPqEHVGUlDEdAd5JiRsiCwBj5OMDqPwe5rZz9o' });
-test.showUser('eddiehinkle').then(function (val) {
-    console.log(val.user);
-});
+__export(require("./lib/goodreads"));
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-//export * from './lib/goodreads';
-import { Goodreads } from './lib/goodreads';
-
-var test = new Goodreads({ key: 'KSXzo3GFar32jnjtiVlQjw', secret: 'hP7UPqEHVGUlDEdAd5JiRsiCwBj5OMDqPwe5rZz9o' });
-
-test.showUser('eddiehinkle').then((val) => {
-    console.log(val.user);
-});
-
+export * from './lib/goodreads';
